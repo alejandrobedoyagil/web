b2fc865edb85fccd67883b294631a47788967c78
alejandrobedoyagil <alejandro.bedoya@flyr.com>
2025-08-28 21:59:53 -0500

    .

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,3 +7,18 @@ async function loadContentAsync(file, target) {
 	catch (err) {
 	}
 }
+async function loadContentAsync2(file, target) {
+	try {
+	    const response = await fetch(file);
+	    const data = await response.text();
+	    document.getElementById(target).innerHTML = data;
+		fetch('data/services.json').then(response => response.json())
+                                   .then(data => {
+                                      const elemento = data.find(item => item.id === "as");
+		                              var content = "$" + elemento.price + document.getElementById("price").innerHTML.trim();
+									  document.getElementById("price").innerHTML = content;
+                                   });
+	}
+	catch (err) {
+	}
+}
